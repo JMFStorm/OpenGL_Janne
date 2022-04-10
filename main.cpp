0dceb12983e1434a42607de84d23912ba0449dc2
Janne Fagerström <jannematiasf@gmail.com>
2022-04-10 14:53:56 +0300

    loadCharacters()

--- main.cpp
+++ main.cpp
@@ -1,14 +1,21 @@
 #include "main.h"
 
-// Function declarations
+struct Character {
+    unsigned int TextureID;  // ID handle of the glyph texture
+    glm::ivec2   Size;       // Size of glyph
+    glm::ivec2   Bearing;    // Offset from baseline to left/top of glyph
+    unsigned int Advance;    // Offset to advance to next glyph
+};
 
 void glfwErrorCallback(int errorCode, const char* description);
 void framebufferSizeCallback(GLFWwindow* window, int width, int height);
+int loadCharacters(const char* fontPath, std::map<char, Character>* characters);
 
 // Main program start
-
 int main()
 {
+    unsigned int VBO, VBO2, VAO, VAO2, EBO;
+
     if (glfwInit() == GLFW_FALSE)
     {
         printf("glfwInit() failed\n");
@@ -48,14 +55,16 @@ int main()
 
     // Load fonts
 
-    FT_Library ft;
-    if (FT_Init_FreeType(&ft))
+    std::map<char, Character> characters;
+
+    if (loadCharacters("fonts/arial.ttf", &characters) != 0)
     {
-        std::cout << "ERROR::FREETYPE: Could not init FreeType Library" << std::endl;
+        std::cout << "Failed to load Glyph characters" << std::endl;
         return -1;
     }
 
     // Load shader program
+
     unsigned int shaderProgram = createShaderProgram("./vertex_shader.shader", "./fragment_shader.shader");
     
     float vertices[] = {
@@ -70,8 +79,6 @@ int main()
         3, 2, 1,  
         3, 0, 1   
     };
-
-    unsigned int VBO, VAO, EBO;
 
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
@@ -141,4 +148,83 @@ void glfwErrorCallback(int errorCode, const char* description)
 void framebufferSizeCallback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
+}
+
+int loadCharacters(const char* fontPath, std::map<char, Character>* characters)
+{
+    FT_Library ft;
+    FT_Face face;
+
+    if (FT_Init_FreeType(&ft))
+    {
+        std::cout << "ERROR::FREETYPE: Could not init FreeType Library" << std::endl;
+        return -1;
+    }
+
+    if (FT_New_Face(ft, "fonts/arial.ttf", 0, &face))
+    {
+        std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
+        return -1;
+    }
+
+    FT_Set_Pixel_Sizes(face, 0, 48);
+
+    if (FT_Load_Char(face, 'X', FT_LOAD_RENDER))
+    {
+        std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
+        return -1;
+    }
+
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1); // disable byte-alignment restriction
+
+    for (unsigned char c = 0; c < 128; c++)
+    {
+        // load character glyph 
+
+        if (FT_Load_Char(face, c, FT_LOAD_RENDER))
+        {
+            std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
+            continue;
+        }
+
+        // generate texture
+
+        unsigned int texture;
+        glGenTextures(1, &texture);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glTexImage2D(
+            GL_TEXTURE_2D,
+            0,
+            GL_RED,
+            face->glyph->bitmap.width,
+            face->glyph->bitmap.rows,
+            0,
+            GL_RED,
+            GL_UNSIGNED_BYTE,
+            face->glyph->bitmap.buffer
+        );
+
+        // set texture options
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        // now store character for later use
+        Character character = {
+            texture,
+            glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
+            glm::ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
+            face->glyph->advance.x
+        };
+
+        characters->insert(std::pair<char, Character>(c, character));
+    }
+
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+    FT_Done_Face(face);
+    FT_Done_FreeType(ft);
+
+    return 0;
 }
