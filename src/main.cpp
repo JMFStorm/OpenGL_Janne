ae490c0d864fbdbce699e6e7c42f748fe57a1a73
Janne Fagerström <jannematiasf@gmail.com>
2022-05-30 19:41:41 +0300

    draw text!

--- src/main.cpp
+++ src/main.cpp
@@ -33,19 +33,19 @@ void JAssert(bool assertion, std::string errorMessage)
 }
 
 struct Character {
-    unsigned int TextureID;  // ID handle of the glyph texture
-    glm::ivec2   Size;       // Size of glyph
-    glm::ivec2   Bearing;    // Offset from baseline to left/top of glyph
-    unsigned int Advance;    // Offset to advance to next glyph
+    unsigned int textureId;  // ID handle of the glyph texture
+    glm::ivec2   size;       // Size of glyph
+    glm::ivec2   bearing;    // Offset from baseline to left/top of glyph
+    unsigned int advance;    // Offset to advance to next glyph
 };
 
-std::map<char, Character>* LoadCharacters(const char* fontPath)
+std::map<GLchar, Character> gCharacters;
+
+void LoadCharacters(const char* fontPath)
 {
     FT_Library ft;
     FT_Face face;
     FT_Error result;
-
-    std::map<char, Character> characters;
 
     result = FT_Init_FreeType(&ft);
     JAssert(result == 0, "ERROR::FREETYPE: Could not init FreeType Library");
@@ -63,14 +63,14 @@ std::map<char, Character>* LoadCharacters(const char* fontPath)
 
     for (unsigned char c = 0; c < 128; c++)
     {
-        // load character glyph 
+        // Load character glyph 
         if (FT_Load_Char(face, c, FT_LOAD_RENDER))
         {
             std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
             continue;
         }
 
-        // generate texture
+        // Generate texture
         unsigned int texture;
         glGenTextures(1, &texture);
         glBindTexture(GL_TEXTURE_2D, texture);
@@ -86,13 +86,13 @@ std::map<char, Character>* LoadCharacters(const char* fontPath)
             face->glyph->bitmap.buffer
         );
 
-        // set texture options
+        // Set texture options
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-        // now store character for later use
+        // Now store character for later use
         Character character = {
             texture,
             glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
@@ -100,15 +100,14 @@ std::map<char, Character>* LoadCharacters(const char* fontPath)
             face->glyph->advance.x
         };
 
-        characters.insert(std::pair<char, Character>(c, character));
+        auto newChar = std::pair<char, Character>(c, character);
+        gCharacters.insert(newChar);
     }
 
-    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+    // glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 
     FT_Done_Face(face);
     FT_Done_FreeType(ft);
-
-    return &characters;
 }
 
 std::string ReadFileToString(std::string filePath)
@@ -482,38 +481,24 @@ namespace Window
 
     GLFWwindow* Create(int width, int height)
     {
-        JAssert(glfwInit() == GLFW_TRUE, "glfwInit() failed");
-
         GLFWwindow* window;
 
         window = glfwCreateWindow(width, height, "LearnOpenGL!", NULL, NULL);
-
         JAssert(window != NULL, "glfwCreateWindow() failed");
+
+        glfwMakeContextCurrent(window);
 
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
         glfwSetErrorCallback(GlfwErrorCallback);
-
-        glfwMakeContextCurrent(window);
-
         glfwSetFramebufferSizeCallback(window, FramebufferSizeCallback);
         glfwSetCursorPosCallback(window, MouseCallback);
         glfwSetScrollCallback(window, ScrollCallback);
 
         // tell GLFW to capture our mouse
         // glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-
-        // Load GLAD context
-        int loaded = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
-        JAssert(loaded == 1, "Failed to initialize OpenGL context GLAD");
-
-        int nrAttributes;
-        glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
-
-        printf("Using OpenGL %d.%d\n", GLVersion.major, GLVersion.minor);
-        printf("Maximum nr of vertex attributes supported: %d\n", nrAttributes);
 
         return window;
     }
@@ -554,12 +539,68 @@ namespace Window
     }
 }
 
+void RenderText(
+    int shader,
+    int vao,
+    int vbo,
+    std::string text,
+    float x,
+    float y,
+    float scale,
+    glm::vec3 color)
+{
+    // activate corresponding render state	
+    Shader::Use(shader);
+    glUniform3f(glGetUniformLocation(shader, "textColor"), color.x, color.y, color.z);
+    glActiveTexture(GL_TEXTURE0);
+
+    VertexArray::Bind(vao);
+
+    // iterate through all characters
+    std::string::const_iterator c;
+    for (c = text.begin(); c != text.end(); c++)
+    {
+        Character ch = gCharacters[*c];
+
+        float xpos = x + ch.bearing.x * scale;
+        float ypos = y - (ch.size.y - ch.bearing.y) * scale;
+
+        float w = ch.size.x * scale;
+        float h = ch.size.y * scale;
+        // update VBO for each character
+        float vertices[6][4] = {
+            { xpos,     ypos + h,   0.0f, 0.0f },
+            { xpos,     ypos,       0.0f, 1.0f },
+            { xpos + w, ypos,       1.0f, 1.0f },
+
+            { xpos,     ypos + h,   0.0f, 0.0f },
+            { xpos + w, ypos,       1.0f, 1.0f },
+            { xpos + w, ypos + h,   1.0f, 0.0f }
+        };
+        // render glyph texture over quad
+        glBindTexture(GL_TEXTURE_2D, ch.textureId);
+        // update content of VBO memory
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        // render quad
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        // now advance cursors for next glyph (note that advance is number of 1/64 pixels)
+        x += (ch.advance >> 6) * scale; // bitshift by 6 to get value in pixels (2^6 = 64)
+    }
+    VertexArray::Unbind();
+    glBindTexture(GL_TEXTURE_2D, 0);
+}
+
 struct ApplicationState
 {
     float currentFrameTime;
     float deltaTime;
     float lastFrame;
 };
+
+#define WINDOW_WIDTH_DEFAULT 1600
+#define WINDOW_HEIGHT_DEFAULT 1200
 
 namespace Application
 {
@@ -579,10 +620,51 @@ namespace Application
         appState.deltaTime = 0.0f;
         appState.lastFrame = 0.0f;
 
-        GLFWwindow* window = Window::Create(1800, 1200);
+        int result;
+
+        result = glfwInit();
+        JAssert(result == GLFW_TRUE, "glfwInit() failed");
+
+        GLFWwindow* window = Window::Create(WINDOW_WIDTH_DEFAULT, WINDOW_HEIGHT_DEFAULT);
+
+        // Load GLAD context
+        result = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+        JAssert(result == 1, "Failed to initialize OpenGL context GLAD");
+
+        glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &result);
+
+        printf("Using OpenGL %d.%d\n", GLVersion.major, GLVersion.minor);
+        printf("Maximum nr of vertex attributes supported: %d\n", result);
 
         // Load text fonts
-        std::map<char, Character>* characters = LoadCharacters("fonts/arial.ttf");
+        LoadCharacters("fonts/arial.ttf");
+
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+        unsigned int textVAO, textVBO;
+        glGenVertexArrays(1, &textVAO);
+        glGenBuffers(1, &textVBO);
+        glBindVertexArray(textVAO);
+        glBindBuffer(GL_ARRAY_BUFFER, textVBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6 * 4, NULL, GL_DYNAMIC_DRAW);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), 0);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindVertexArray(0);
+
+        unsigned int textShader1 = Shader::Create(
+            "./shaders/font_vertex_shader.shader",
+            "./shaders/font_fragment_shader.shader");
+
+        glm::mat4 projection = glm::ortho(
+            0.0f,
+            (float)WINDOW_WIDTH_DEFAULT,
+            0.0f,
+            (float)WINDOW_HEIGHT_DEFAULT);
+
+        Shader::Use(textShader1);
+        glUniformMatrix4fv(glGetUniformLocation(textShader1, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
 
         // Load texture1
         unsigned int texture1 = Texture::Create("./images/container.jpg", false);
@@ -646,11 +728,30 @@ namespace Application
             Shader::Use(shader1);
 
             glActiveTexture(GL_TEXTURE0);
-
             VertexArray::Bind(vertexArrayObject);
 
             int elementsCount = indices.size();
             glDrawElements(GL_TRIANGLES, elementsCount, GL_UNSIGNED_INT, 0);
+
+            RenderText(
+                textShader1,
+                textVAO,
+                textVBO, 
+                "This is sample text", 
+                25.0f, 
+                25.0f, 
+                1.0f, 
+                glm::vec3(0.5, 0.8f, 0.2f));
+
+            RenderText(
+                textShader1, 
+                textVAO, 
+                textVBO, 
+                "(C) LearnOpenGL.com", 
+                540.0f,
+                570.0f,
+                0.5f,
+                glm::vec3(0.3, 0.7f, 0.9f));
 
             Window::SwapScreenBuffer(window);
         }
